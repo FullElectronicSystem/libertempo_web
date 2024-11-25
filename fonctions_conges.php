783d6ae45d81c650766fc807fdc3c1b037f0f367
Guillaume Fege <guillaume.fege@fesys.fr>
2024-11-25 11:53:36 +0100

    Allow "responsables" to add conges

--- fonctions_conges.php
+++ fonctions_conges.php
@@ -88,10 +88,10 @@ function saisie_nouveau_conges2($user_login, $year_calendrier_saisie_debut, $moi
     $already_checked = false;
 
     $return .= '<div class="row type-conges">';
-    // si le user a droit de saisir une demande de conges ET si on est PAS dans une fenetre de responsable
+    // si le user a droit de saisir une demande de conges 
     // OU si le user n'a pas droit de saisir une demande de conges ET si on est dans une fenetre de responsable
     // OU si le user est un RH ou un admin
-    if( ( $_SESSION['config']['user_saisie_demande'] && $user_login==$_SESSION['userlogin'] ) || ( $_SESSION['config']['user_saisie_demande']==FALSE && $user_login!=$_SESSION['userlogin'] ) || is_hr($_SESSION['userlogin']) || is_admin($_SESSION['userlogin']) )
+    if( ( $_SESSION['config']['user_saisie_demande'] ) || ( $_SESSION['config']['user_saisie_demande']==FALSE && $user_login!=$_SESSION['userlogin'] ) || is_hr($_SESSION['userlogin']) || is_admin($_SESSION['userlogin']) )
     {
         // congés
         $return .= '<div class="col-md-4">';
@@ -126,10 +126,10 @@ function saisie_nouveau_conges2($user_login, $year_calendrier_saisie_debut, $moi
         $return .= '</div>';
     }
 
-    // si le user a droit de saisir une demande de conges ET si on est PAS dans une fenetre de responsable
+    // si le user a droit de saisir une demande de conges 
     // OU si le user n'a pas droit de saisir une demande de conges ET si on est dans une fenetre de responsable
 	// OU si le user est un RH ou un admin
-    if( ($_SESSION['config']['gestion_conges_exceptionnels']) && ((($_SESSION['config']['user_saisie_demande'])&&($user_login==$_SESSION['userlogin'])) || (($_SESSION['config']['user_saisie_demande']==FALSE)&&($user_login!=$_SESSION['userlogin'])) ) || is_hr($_SESSION['userlogin']) || is_admin($_SESSION['userlogin']))
+    if( ($_SESSION['config']['gestion_conges_exceptionnels']) && ((($_SESSION['config']['user_saisie_demande'])) || (($_SESSION['config']['user_saisie_demande']==FALSE)&&($user_login!=$_SESSION['userlogin'])) ) || is_hr($_SESSION['userlogin']) || is_admin($_SESSION['userlogin']))
     {
         // congés exceptionnels
         $return .= '<div class="col-md-4">';
